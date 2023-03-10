aaeb67b576a36f4350907e31cf1bb2d3a1ad4bc4
IlyassAIDI <aidi_ilyass@emsi-edu.ma>
2023-03-10 18:01:36 +0100

    Modification des dossiers

--- pap-backend/src/main/java/com/pap/papbackened/PapBackenedApplication.java
+++ pap-backend/src/main/java/com/pap/papbackened/PapBackenedApplication.java
@@ -1,13 +1,13 @@
-package com.example.papboutiquev1;
+package com.pap.papbackened;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PapBoutiqueV1Application {
+public class PapBackenedApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PapBoutiqueV1Application.class, args);
+        SpringApplication.run(PapBackenedApplication.class, args);
     }
 
 }
