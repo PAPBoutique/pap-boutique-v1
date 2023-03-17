72701f4583accb9652cffe4d196319023ce2ff02
IlyassAIDI <aidi_ilyass@emsi-edu.ma>
2023-03-17 11:59:09 +0100

    Changing the Java version from 17 to 11

--- pap-backend/src/test/java/com/pap/papbackend/PapbackendApplicationTests.java
+++ pap-backend/src/test/java/com/pap/papbackend/PapbackendApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PapbackendApplicationTests {
+class PapBackendApplicationTests {
 
     @Test
     void contextLoads() {
