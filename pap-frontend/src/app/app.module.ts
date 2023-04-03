8a13512a9329693f62ff617b554745533ec0f37c
Fayçal ELJARRUDE <faycal.eljarrude@sii-maroc.com>
2023-04-03 15:25:20 +0000

    PB-3 Project structure

--- pap-frontend/src/app/app.module.ts
+++ pap-frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateProductComponent } from './product/page/create-product/create-product.component';
+import { ListProductComponent } from './product/page/list-product/list-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateProductComponent,
+    ListProductComponent
   ],
   imports: [
     BrowserModule,
