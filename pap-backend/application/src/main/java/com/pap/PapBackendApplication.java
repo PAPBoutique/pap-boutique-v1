c93d3b74395f1f9715ee1b0ecc3a8a1b35c2b0a6
Fayçal ELJARRUDE <faycal.eljarrude@sii-maroc.com>
2023-04-03 15:25:20 +0000

    PB-3 Project structure

--- pap-backend/application/src/main/java/com/pap/PapBackendApplication.java
+++ pap-backend/application/src/main/java/com/pap/PapBackendApplication.java
@@ -1,13 +1,11 @@
-package com.pap.papbackend;
+package com.pap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PapBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PapBackendApplication.class, args);
     }
-
 }
