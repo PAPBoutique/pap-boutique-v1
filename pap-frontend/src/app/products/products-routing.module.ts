3beda4daae33f3d1e118433509d3a3375f13f42f
Fayçal ELJARRUDE <faycal.eljarrude@sii-maroc.com>
2023-04-04 15:46:44 +0000

    PB-3 code-review 03

--- pap-frontend/src/app/products/products-routing.module.ts
+++ pap-frontend/src/app/products/products-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateProductComponent } from './page/create-product/create-product.component';
 import { ListProductsComponent } from './page/list-products/list-products.component';
+import { PageNotFoundComponent } from '../shared-components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path:'/create' , component: CreateProductComponent},
-  {path :'/list' , component: ListProductsComponent}
+  {path:'create' , component: CreateProductComponent},
+  {path :'list' , component: ListProductsComponent},
+  {path :'' , redirectTo:'list' , pathMatch:'full'},
+  { path:'**' , component:  PageNotFoundComponent}
 ];
 
 @NgModule({
